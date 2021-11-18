f0a6ef1622b007f603e385b0bff9f6cd8fe7d247
dugunhee <rjsdl2210@naver.com>
2021-11-18 18:16:48 +0900

    long time no see

--- NAVER/1.cpp
+++ NAVER/1.cpp
@@ -2,19 +2,5 @@
 #include<string>
 using namespace std;
 int main(){
-    int arr[26] = {0,};
-    string str = "dbbaaaac";
-    for(int i=0;i<str.size();i++)
-    {
-        if(arr[str[i]-'a'] == 0){
-            arr[str[i]-'a'] = 1;
-        }
-        else continue;
-    }
-    string ans = "";
-    for(int i=0;i<26;i++)
-    {
-        if(arr[i]!=0) ans += ('a'+i);
-    }
-    cout<<ans<<endl;
+    
 }
