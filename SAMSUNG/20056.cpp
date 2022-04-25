fa8e38dbb9e5cf6329c75d7a021ce4cf23a735e1
dugunhee <rjsdl2210@naver.com>
2022-04-25 20:33:05 +0900

    SAMSUNG

--- SAMSUNG/20056.cpp
+++ SAMSUNG/20056.cpp
@@ -1,106 +1,118 @@
 #include<iostream>
-#include<string.h>
-#include<vector>
-#include<utility>
-#include<algorithm>
 #include<queue>
+#include<utility>
 using namespace std;
-vector<pair<int, int> > vec;
+struct fireball{
+    int r,c;
+    int m;
+    int d;
+    int s;
+};
 int dx[8] = {-1,-1,0,1,1,1,0,-1};
 int dy[8] = {0,1,1,1,0,-1,-1,-1};
-int board[51][51];
-queue<int> q[51][51];
+queue<fireball> q[51][51];
+int qsize[51][51];
 int N,M,K;
-int fbcnt;
-struct fireball{
-    int r;
-    int c;
-    int m;
-    int s;
-    int d;
-    int valid;
-} fb[100000];
-void move(int idx, int r,int c, int m, int s, int d){
-    board[r][c]--;
-    q[r][c].pop();
-    for(int i=0;i<s;i++){
+void get_qsize(){
+    for(int i=1;i<=N;i++){
+        for(int j=1;j<=N;j++){
+            qsize[i][j] = q[i][j].size();
+            cout<<qsize[i][j]<<' ';
+        }
+        cout<<'\n';
+    }
+    cout<<'\n';
+}
+pair<int,int> move_dir(int r, int c, int d, int s){
+    cout<<"rcds"<<r<<' '<<c<<' '<<d<<' '<<s<<'\n';
+    while(s--){
         r += dx[d];
         c += dy[d];
-        if(r<0) r+=N;
-        if(c<0) c+=N;
-        r %= N;
-        c %= N;
+        cout<<r<<' ' <<c<<'\n';
+        if(r>N) r = 1;
+        else if(r==0) r = N;
+        if(c>N) c = 1;
+        else if(c==0) c = N;
     }
-    board[r][c]++;
-    q[r][c].push(idx);
-    fb[idx].r = r;
-    fb[idx].c = c;
+    return {r,c};
 }
+void move_fireball(){
+    get_qsize();
+    for(int i=1;i<=N;i++){
+        for(int j=1;j<=N;j++){
+            for(int k=0;k<qsize[i][j];k++){
+                fireball fb = q[i][j].front();
+                q[i][j].pop();
+                pair<int,int> p = move_dir(i,j,fb.d,fb.s);
+                int r = p.first;
+                int c = p.second;
+                //cout<<"r c"<<r<<' '<<c<<'\n';
+                q[r][c].push(fb);
+            }
+        }
+    }
+}
+void split_fireball(){
+    get_qsize();
+    for(int i=1;i<=N;i++){
+        for(int j=1;j<=N;j++){
+            if(qsize[i][j]<2) continue;
+            int new_m = 0;
+            int new_s = 0;
+            int new_d[4]={0,};
+            int oddcnt=0, evencnt=0;
+            for(int k=0;k<qsize[i][j];k++){
+                fireball fb = q[i][j].front();
+                q[i][j].pop();
+                new_m += fb.m;
+                new_s += fb.s;
+                if(fb.d%2 == 0) evencnt++;
+                if(fb.d%2 == 1) oddcnt++;
+            }
+            new_m /= 5;
+            if(new_m == 0) continue;
+            new_s /= qsize[i][j];
+            if(evencnt == qsize[i][j] || oddcnt == qsize[i][j]){
+                new_d[0] = 0; new_d[1] = 2; new_d[2] = 4; new_d[3] = 6;
+            }
+            else{
+                new_d[0] = 1; new_d[1] = 3; new_d[2] = 5; new_d[3] = 7;
+            }
+            for(int k=0;k<4;k++){
+                fireball fb;
+                fb.r = i; fb.c = j;
+                fb.d = new_d[k];
+                fb.m = new_m;
+                fb.s = new_s;
+                q[i][j].push(fb);
+            }
+        }
+    }
+}
+
 int main()
 {
-    memset(board,0,sizeof(board));
     cin>>N>>M>>K;
-    fbcnt = M;
+    int r,c,m,d,s;
     for(int i=0;i<M;i++){
-        cin>>fb[i].r>>fb[i].c>>fb[i].m>>fb[i].s>>fb[i].d;
-        fb[i].r--;
-        fb[i].c--;
-        board[fb[i].r][fb[i].c] = 1;
-        fb[i].valid = 1;
-        q[fb[i].r][fb[i].c].push(i);
+        cin>>r>>c>>m>>s>>d;
+        fireball fb;
+        fb.r = r; fb.c = c; fb.m = m; fb.d = d; fb.s = s;
+        q[r][c].push(fb);
     }
-    for(int x=0;x<K;x++){
-        for(int i=0;i<fbcnt;i++){
-            if(fb[i].valid == 1)move(i,fb[i].r,fb[i].c,fb[i].m,fb[i].s,fb[i].d);
-        }
-        
-        vec.clear();
-        for(int i=0;i<N;i++){
-            for(int j=0;j<N;j++){
-                if(board[i][j] >= 2){
-                    vec.push_back(make_pair(i,j));
-                }
+    while(K--){
+        move_fireball();
+        split_fireball();
+    }
+    get_qsize();
+    long long ans = 0;
+    for(int i=1;i<=N;i++){
+        for(int j=1;j<=N;j++){
+            for(int k=0;k<qsize[i][j];k++){
+                ans += q[i][j].front().m;
+                q[i][j].pop();
             }
         }
-        for(int i=0;i<vec.size();i++){
-            int msum = 0;
-            int ssum = 0;
-            int evencnt = 0;
-            int oddcnt = 0;
-            int size = q[vec[i].first][vec[i].second].size();
-            for(int j=0;j<size;j++){
-                msum += fb[q[vec[i].first][vec[i].second].front()].m;
-                ssum += fb[q[vec[i].first][vec[i].second].front()].s;
-                if(fb[q[vec[i].first][vec[i].second].front()].d%2 == 0){
-                    evencnt++;
-                }
-                else oddcnt++;
-                fb[q[vec[i].first][vec[i].second].front()].valid = 0;
-                q[vec[i].first][vec[i].second].pop();
-                board[vec[i].first][vec[i].second]--;
-            }
-            msum = (msum/5);
-            if(msum == 0)continue;
-            ssum = (ssum/size);
-            for(int k=0;k<4;k++){
-                fb[fbcnt+k].r = vec[i].first;
-                fb[fbcnt+k].c = vec[i].second;
-                fb[fbcnt+k].m = msum;
-                fb[fbcnt+k].s = ssum;
-                if(oddcnt == size || evencnt == size)fb[fbcnt+k].d = 2*k;
-                else fb[fbcnt+k].d = 2*k+1;
-                q[vec[i].first][vec[i].second].push(fbcnt+k);
-                fb[fbcnt+k].valid = 1;
-            }
-            fbcnt+=4;
-            board[vec[i].first][vec[i].second]+=4;
-        }
     }
-    int sum = 0;
-    for(int i=0;i<fbcnt;i++){
-        if(fb[i].valid == 1){
-            sum += fb[i].m;
-        }
-    }
-    cout<<sum<<endl;
+    cout<<ans<<'\n';
 }
